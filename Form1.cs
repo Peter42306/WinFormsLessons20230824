db43fa83b9481764c7208040dccfda0876221599
Peter42306 <pzalizko@gmail.com>
2023-08-24 18:08:04 +0300

    WinForms, creation of 1d array of buttons

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
-﻿namespace WinFormsApp20230824
+﻿using System.Data.Common;
+
+namespace WinFormsApp20230824
 {
     public partial class Form1 : Form
     {
@@ -6,20 +8,43 @@
         {
             InitializeComponent();
 
-            // Создание и настройка кнопки
-            Button b = new Button();
-            b.Width = 50;
-            b.Height = 50;
-            b.Location = new Point(10, 10);
-            b.Text = "1";
-            b.MouseDown += B_MouseDown; // Подписка на событие "MouseDown"
-            b.Parent = this; // Установка текущей формы в качестве родительского элемента кнопки
+            // массив кнопок
+
+            int buttonWidth = 50;
+            int buttonHeight = 50;
+            int distanceBetweenButtons = 10;
+            int distanceFirstButtonFromLeft = 10;
+
+            int numberOfButtons = 12;
+
+            for (int i = 0; i < numberOfButtons; i++)
+            {
+                // Создание экземпляра Button
+                Button button = new Button();
+
+                // Настройка размеров кнопки
+                button.Size = new Size(buttonWidth, buttonHeight);
+
+                // Расположение кнопки на форме, в зависимости от индекса i
+                button.Location = new Point(
+                    distanceFirstButtonFromLeft + i * (buttonWidth + distanceBetweenButtons),
+                    distanceBetweenButtons
+                    );
+
+                // Текст кнопки
+                button.Text = (i + 1).ToString();
+
+                // Подписка на событие "MouseDown"
+                button.MouseDown += B_MouseDown; 
+
+                button.Parent = this;
+            }
         }
 
         private void B_MouseDown(object? sender, MouseEventArgs e)
         {
-            Button? b = sender as Button; // Преобразование отправителя в тип Button
-            b.Visible = false; // Скрытие кнопки при нажатии на неё
+            Button? button = sender as Button; // Преобразование отправителя в тип Button
+            button.Visible = false; // Скрытие кнопки при нажатии на неё
         }
 
         private void Form1_Load(object sender, EventArgs e)
--- Form2.cs
+++ Form2.cs
@@ -15,6 +15,34 @@ namespace WinFormsApp20230824
         public Form2()
         {
             InitializeComponent();
+
+            // двумерный массив кнопок
+
+            int buttonWidth = 50;
+            int buttonHeight = 50;
+            int distanceBetweenButtons = 10;
+            int distanceFirstButtonFromLeft = 10;
+            int distanceFromTop = 10;
+
+            int x, y;
+            int numberOfRows = 8;
+            int numberOfColumns = 14;
+
+            for (int row = 0; row < numberOfRows; row++)
+            {
+                for (int column = 0; column < numberOfColumns; column++)
+                {
+                    Button button = new Button();
+                    button.Size = new Size(buttonWidth, buttonHeight);
+
+                    x = distanceFirstButtonFromLeft + column * (buttonWidth + distanceBetweenButtons);
+                    y = distanceFromTop + row * (buttonHeight + distanceBetweenButtons);
+
+                    button.Location = new Point(x, y);
+                    button.Text = ((row * numberOfColumns) + column + 1).ToString();
+                    button.Parent = this;
+                }
+            }
         }
 
         private void Form2_Load(object sender, EventArgs e)
