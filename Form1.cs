55169d4dbe37de5b077ec9069ab787b06c807069
Peter42306 <pzalizko@gmail.com>
2023-08-24 17:38:52 +0300

    new branch: Winforms, button + invisible button

--- Form1.cs
+++ Form1.cs
@@ -11,8 +11,15 @@
             b.Width = 50;
             b.Height = 50;
             b.Location = new Point(10, 10);
-            b.Text = "1";            
+            b.Text = "1";
+            b.MouseDown += B_MouseDown; // Подписка на событие "MouseDown"
             b.Parent = this; // Установка текущей формы в качестве родительского элемента кнопки
+        }
+
+        private void B_MouseDown(object? sender, MouseEventArgs e)
+        {
+            Button? b = sender as Button; // Преобразование отправителя в тип Button
+            b.Visible = false; // Скрытие кнопки при нажатии на неё
         }
 
         private void Form1_Load(object sender, EventArgs e)
